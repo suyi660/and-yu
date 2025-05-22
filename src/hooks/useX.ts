107799ae8bb1f802b59fe22fee1a3b50a48bc531
suyi <suyi660@gmail.com>
2025-05-22 15:36:37 +0700

    useX 统一column时，过滤hidden为true

--- src/hooks/useX.ts
+++ src/hooks/useX.ts
@@ -1,12 +1,12 @@
 
 import { useMemo } from 'react';
 
-
 export default function useX(column: any[] | ((data: any) => any[])): number {
     if (!Array.isArray(column)) return 0;
     return useMemo(() => {
         if (!column.length) return 0;
-        return column.reduce((a, b) => {
+        const columns = column.filter((item: any) => !item.hidden);
+        return columns.reduce((a, b) => {
             return a + parseInt(b.width || 0);
         }, 0);
     }, [column]);
