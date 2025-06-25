5a5b0fe17bfd19f1e1e9151d02ad71b39da95eaf
suyi <suyi660@gmail.com>
2025-06-25 19:48:48 +0700

    table: column,alert函数式，callback增加 search参数,

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -90,12 +90,12 @@ const ProTable = <T extends Record<string, any>>(props: ProTableProps<T>) => {
 
     const { dataSource, total, column, renderAlert } = useMemo(() => {
         return {
-            column: typeof columns === 'function' ? columns(data as any) : columns,
+            column: typeof columns === 'function' ? columns(data as any, search,) : columns,
             dataSource: getDataSource<T>(data as any, dataKey),
-            renderAlert: typeof alert === 'function' ? alert(data as any) : alert,
+            renderAlert: typeof alert === 'function' ? alert(data as any, search,) : alert,
             total: getTotal(totalKey, data)
         };
-    }, [columns, data, dataKey, totalKey]);
+    }, [columns, data, dataKey, totalKey, search]);
 
     const onSearch = () => {
         if (formItems) {
