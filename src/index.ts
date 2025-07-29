299484980a959ddc08d60dee6fbb5929a7dc0dbe
suyi <suyi660@gmail.com>
2025-07-29 15:11:45 +0700

    恢复useX导出

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useFetch, } from './hooks/useFetch'
+export { default as useX, } from './hooks/useX'
 export { default as Table } from './table'
 export { default as Rq, rq, downloadfile } from './fetch'
