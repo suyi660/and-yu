1e58b5791fc5a70c450067038a7ecb38354deeb7
suyi <suyi660@gmail.com>
2025-08-25 16:47:54 +0700

    fix: table组件导出命名修复

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { default as useFetch, } from './hooks/useFetch'
 export { default as useMutation, } from './hooks/useMutation'
 export { default as useQuery, } from './hooks/useQuery'
 export { default as useX, } from './hooks/useX'
-export { default as table } from './table'
+export { default as Table } from './table'
 export { default as Rq, rq, downloadfile } from './fetch'
 export type * from './types'
 
