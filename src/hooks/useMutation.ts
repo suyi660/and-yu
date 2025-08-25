5eb88f1160b19bbb6e20a55137e1aa30dd4d1108
suyi <suyi660@gmail.com>
2025-08-25 16:45:03 +0700

    feat: table_query重命名成table, 补齐useMutation Options类型传导

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -4,14 +4,14 @@ import { rq, } from '../fetch'
 import { isObject, } from '../utils/util'
 import type { UseMutationResult, UseMutationOptions } from '@tanstack/react-query'
 
-interface Options extends UseMutationOptions {
+interface Options<TData> extends UseMutationOptions<TData> {
     url: string;
     method?: string;
     json?: unknown;
     headers?: Record<string, string>;
 }
 
-export default function useMutationHooks<TData = unknown>(options: Options): UseMutationResult<TData> {
+export default function useMutationHooks<TData = unknown>(options: Options<TData>): UseMutationResult<TData> {
     const {
         url,
         json,
