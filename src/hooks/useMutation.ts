7f00eea35cdfaeb209b417bfe1eeda64fa148cba
suyi <suyi660@gmail.com>
2025-10-18 08:21:33 +0700

    update: useFetch version

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -4,14 +4,18 @@ import { rq, } from '../fetch'
 import { isObject, } from '../utils/util'
 import type { UseMutationResult, UseMutationOptions } from '@tanstack/react-query'
 
-interface Options<TData> extends UseMutationOptions<TData> {
+
+interface Options<TData, TError = unknown, TVariables = void, TOnMutateResult = unknown>
+    extends UseMutationOptions<TData, TError, TVariables, TOnMutateResult> {
     url: string;
     method?: string;
     json?: unknown;
     headers?: Record<string, string>;
 }
 
-export default function useMutationHooks<TData = unknown>(options: Options<TData>): UseMutationResult<TData> {
+export default function useMutationHooks<TData = unknown, TError = unknown, TVariables = void, TOnMutateResult = unknown>(
+    options: Options<TData, TError, TVariables, TOnMutateResult>
+): UseMutationResult<TData, TError, TVariables, TOnMutateResult> {
     const {
         url,
         json,
@@ -19,8 +23,8 @@ export default function useMutationHooks<TData = unknown>(options: Options<TData
         headers,
         ...others
     } = options;
-    return useMutation({
-        mutationFn(data: unknown) {
+    return useMutation<TData, TError, TVariables, TOnMutateResult>({
+        mutationFn(data?: unknown): Promise<TData> {
             if (isObject(data) && Object.prototype.hasOwnProperty.call(data, "nativeEvent")) {
                 data = undefined;
             }
